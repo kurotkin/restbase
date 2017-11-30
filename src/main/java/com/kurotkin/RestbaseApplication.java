2f467bdb3e9f04522f7959ed53cecdc3962dd347
kurotkin <kurotkin@users.noreply.github.com>
2017-11-30 16:50:11 +0300

    Initial commit

---
+++ src/main/java/com/kurotkin/RestbaseApplication.java
@@ -0,0 +1,12 @@
+package com.kurotkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbaseApplication.class, args);
+	}
+}
